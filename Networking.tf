df0f07b45cfe08bd2cbe4c79f4cb667cfd13977c
ArielLeza <ariel.leza@gmail.com>
2021-12-02 19:44:09 -0600

    edit dn for vrf

--- Networking.tf
+++ Networking.tf
@@ -5,7 +5,7 @@ data "aci_vrf" "inb" {
 
 resource "aci_bridge_domain" "bd_data" {
         tenant_dn                   = data.aci_tenant.mgmt.id
-        relation_fv_rs_ctx       = aci_vrf.inb.id
+        relation_fv_rs_ctx       = data.aci_vrf.inb.id
         name                        = "bd_data"
         arp_flood                   = "yes"
         host_based_routing          = "yes"
@@ -13,7 +13,7 @@ resource "aci_bridge_domain" "bd_data" {
 
 resource "aci_bridge_domain" "bd_mgmt" {
         tenant_dn                   = data.aci_tenant.mgmt.id
-        relation_fv_rs_ctx       = aci_vrf.inb.id
+        relation_fv_rs_ctx       = data.aci_vrf.inb.id
         name                        = "bd_mgmt"
         arp_flood                   = "yes"
         host_based_routing          = "yes"
