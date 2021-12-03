eb695fb9b649f241bd7639850f4f8226ed22b1b3
ArielLeza <ariel.leza@gmail.com>
2021-12-02 19:42:37 -0600

    edit wrong dn

--- Networking.tf
+++ Networking.tf
@@ -4,15 +4,15 @@ data "aci_vrf" "inb" {
 }
 
 resource "aci_bridge_domain" "bd_data" {
-        tenant_dn                   = data.aci_tenant.common.id
-        relation_fv_rs_ctx       = aci_vrf.vrf1.id
+        tenant_dn                   = data.aci_tenant.mgmt.id
+        relation_fv_rs_ctx       = aci_vrf.inb.id
         name                        = "bd_data"
         arp_flood                   = "yes"
         host_based_routing          = "yes"
     }
 
 resource "aci_bridge_domain" "bd_mgmt" {
-        tenant_dn                   = data.aci_tenant.common.id
+        tenant_dn                   = data.aci_tenant.mgmt.id
         relation_fv_rs_ctx       = aci_vrf.inb.id
         name                        = "bd_mgmt"
         arp_flood                   = "yes"
